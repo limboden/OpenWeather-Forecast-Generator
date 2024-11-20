d6e1398c49285ae551a06066a6fd52240143e057
limboden <imbodenlu@gmail.com>
2024-11-19 22:54:27 -0500

    im pretty lost with what all these different methods do. there is no documenntation stating what each one should do

--- Develop/server/src/service/weatherService.ts
+++ Develop/server/src/service/weatherService.ts
@@ -3,6 +3,12 @@ dotenv.config();
 const API_KEY = process.env.API_KEY;
 const BASE_URL = process.env.API_BASE_URL;
 // TODO: Define an interface for the Coordinates object
+
+interface Coordinates {
+  lat: number,
+  lon: number
+}
+
 // TODO: Define a class for the Weather object
 
 // TODO: Complete the WeatherService class
@@ -12,20 +18,33 @@ class WeatherService {
   API_KEY = API_KEY;
 
   // TODO: Create fetchLocationData method
-  private async fetchLocationData(cityName: string, stateCode: number, countryCode: string, limit: string) {
-    await fetch(`${BASE_URL}/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=${API_KEY}`)
+  private async fetchLocationData(query: string) {
+    await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${API_KEY}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const latFromData = data[0].lat;
+        const lonFromData = data[0].lon
+      });
+  }
+
+  // TODO: Create destructureLocationData method
+  private destructureLocationData(locationData: Coordinates): Coordinates {
+    const { lat, lon } = locationData;
+    return { lat, lon }
+  }
+  // TODO: Create buildGeocodeQuery method
+  // private buildGeocodeQuery(): string {}
+  // TODO: Create buildWeatherQuery method
+  private buildWeatherQuery(coordinates: Coordinates): string {
+    const coords = this.destructureLocationData(coordinates);
+
+    fetch(`${BASE_URL}/geo/1.0/reverse?lat=${coords.lat}&lon=${coords.lon}&limit=1&appid=${API_KEY}`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
       });
   }
-
-  // TODO: Create destructureLocationData method
-  // private destructureLocationData(locationData: Coordinates): Coordinates {}
-  // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
-  // TODO: Create buildWeatherQuery method
-  // private buildWeatherQuery(coordinates: Coordinates): string {}
   // TODO: Create fetchAndDestructureLocationData method
   // private async fetchAndDestructureLocationData() {}
   // TODO: Create fetchWeatherData method
@@ -35,7 +54,9 @@ class WeatherService {
   // TODO: Complete buildForecastArray method
   // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
   // TODO: Complete getWeatherForCity method
-  // async getWeatherForCity(city: string) {}
+  async getWeatherForCity(city: string) {
+
+  }
 
 
 }
