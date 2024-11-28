79ebe156600dec301d54c3b3132f5f0de5536e23
limboden <imbodenlu@gmail.com>
2024-11-28 12:07:28 -0500

    added to server.ts

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -10,10 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-app.get('*', (_req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
-});
+app.use(express.static('../client/dist'));
+
 // TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // TODO: Implement middleware to connect the routes
 app.use(routes);
