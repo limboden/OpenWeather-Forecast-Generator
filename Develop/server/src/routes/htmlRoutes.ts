7b16ee8505ef835bd2923f8cbccd9e93d4c6d01d
limboden <imbodenlu@gmail.com>
2024-11-28 09:31:50 -0500

    htmlroutes file

--- Develop/server/src/routes/htmlRoutes.ts
+++ Develop/server/src/routes/htmlRoutes.ts
@@ -6,8 +6,10 @@ const __dirname = path.dirname(__filename);
 const router = Router();
 
 // TODO: Define route to serve index.html
-router.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+router.get('/', (_req, res) => {
+  const indexRoute = path.join(__dirname, '..', '..', 'client', 'dist', 'index.html');
+  res.sendFile(indexRoute);
 });
+
 
 export default router;
